588d4977e8ab6090fe016d2162f1110051598c3b
armancurr <armancurr@gmail.com>
2026-07-31 16:57:25 +0530

    fix: align rail pattern tiles to frame bounds

--- src/components/side-pattern.tsx
+++ src/components/side-pattern.tsx
@@ -3,11 +3,11 @@ export function SidePattern() {
     <>
       <span
         aria-hidden="true"
-        class="rail-pattern pointer-events-none absolute top-[-1px] right-[calc(100%+1px)] bottom-[-1px] w-screen"
+        class="rail-pattern pointer-events-none absolute top-0 right-[calc(100%+1px)]"
       />
       <span
         aria-hidden="true"
-        class="rail-pattern pointer-events-none absolute top-[-1px] bottom-[-1px] left-[calc(100%+1px)] w-screen"
+        class="rail-pattern pointer-events-none absolute top-0 left-[calc(100%+1px)]"
       />
     </>
   );
